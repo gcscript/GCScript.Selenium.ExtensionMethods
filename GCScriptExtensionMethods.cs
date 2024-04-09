abe76c6326921aed1c06c7ce308ae2f0f01c5882
Gustavo Costa <gcs5stars@gmail.com>
2024-04-09 15:06:16 -0300

    Bug fixes and performance improvements

--- GCScriptExtensionMethods.cs
+++ GCScriptExtensionMethods.cs
@@ -1,4 +1,7 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.ObjectModel;
 using System.Text;
 
@@ -30,25 +33,25 @@ public static class GCScriptExtensionMethods
     public static ReadOnlyCollection<IWebElement> GetElementsByTagName(this IWebElement element, string tagName) => element.FindElements(By.TagName(tagName));
     public static ReadOnlyCollection<IWebElement> GetElementsByLinkText(this IWebElement element, string linkText) => element.FindElements(By.LinkText(linkText));
 
-    public static void WaitElementIsVisibleById(this IWebDriver driver, string id, int seconds = 15)
+    public static async Task WaitElementIsVisibleById(this IWebDriver driver, string id, int seconds = 15)
     {
-        driver.WaitElementIsVisible(x => x.FindElement(By.Id(id)), seconds);
+        await driver.WaitElementIsVisible(x => x.FindElement(By.Id(id)), seconds);
     }
-    public static void WaitElementIsVisibleBySelector(this IWebDriver driver, string selector, int seconds = 15)
+    public static async Task WaitElementIsVisibleBySelector(this IWebDriver driver, string selector, int seconds = 15)
     {
-        driver.WaitElementIsVisible(x => x.FindElement(By.CssSelector(selector)), seconds);
+        await driver.WaitElementIsVisible(x => x.FindElement(By.CssSelector(selector)), seconds);
     }
-    public static void WaitElementIsVisibleByXPath(this IWebDriver driver, string xpath, int seconds = 15)
+    public static async Task WaitElementIsVisibleByXPath(this IWebDriver driver, string xpath, int seconds = 15)
     {
-        driver.WaitElementIsVisible(x => x.FindElement(By.XPath(xpath)), seconds);
+        await driver.WaitElementIsVisible(x => x.FindElement(By.XPath(xpath)), seconds);
     }
-    public static void WaitElementIsVisibleByTagName(this IWebDriver driver, string tagName, int seconds = 15)
+    public static async Task WaitElementIsVisibleByTagName(this IWebDriver driver, string tagName, int seconds = 15)
     {
-        driver.WaitElementIsVisible(x => x.FindElement(By.TagName(tagName)), seconds);
+        await driver.WaitElementIsVisible(x => x.FindElement(By.TagName(tagName)), seconds);
     }
-    public static void WaitElementIsVisibleByLinkText(this IWebDriver driver, string linkText, int seconds = 15)
+    public static async Task WaitElementIsVisibleByLinkText(this IWebDriver driver, string linkText, int seconds = 15)
     {
-        driver.WaitElementIsVisible(x => x.FindElement(By.LinkText(linkText)), seconds);
+        await driver.WaitElementIsVisible(x => x.FindElement(By.LinkText(linkText)), seconds);
     }
     private static async Task WaitElementIsVisible<T>(this IWebDriver driver, Func<IWebDriver, T> getElement, int seconds = 15) where T : IWebElement
     {
@@ -68,25 +71,25 @@ public static class GCScriptExtensionMethods
         throw new Exception($"Limit of {seconds} seconds exceeded in [WaitElementIsVisible]");
     }
 
-    public static void WaitElementExistsById(this IWebDriver driver, string id, int seconds = 15)
+    public static async Task WaitElementExistsById(this IWebDriver driver, string id, int seconds = 15)
     {
-        driver.WaitElementExists(x => x.FindElement(By.Id(id)), seconds);
+        await driver.WaitElementExists(x => x.FindElement(By.Id(id)), seconds);
     }
-    public static void WaitElementExistsBySelector(this IWebDriver driver, string selector, int seconds = 15)
+    public static async Task WaitElementExistsBySelector(this IWebDriver driver, string selector, int seconds = 15)
     {
-        driver.WaitElementExists(x => x.FindElement(By.CssSelector(selector)), seconds);
+        await driver.WaitElementExists(x => x.FindElement(By.CssSelector(selector)), seconds);
     }
-    public static void WaitElementExistsByXPath(this IWebDriver driver, string xpath, int seconds = 15)
+    public static async Task WaitElementExistsByXPath(this IWebDriver driver, string xpath, int seconds = 15)
     {
-        driver.WaitElementExists(x => x.FindElement(By.XPath(xpath)), seconds);
+        await driver.WaitElementExists(x => x.FindElement(By.XPath(xpath)), seconds);
     }
-    public static void WaitElementExistsByTagName(this IWebDriver driver, string tagName, int seconds = 15)
+    public static async Task WaitElementExistsByTagName(this IWebDriver driver, string tagName, int seconds = 15)
     {
-        driver.WaitElementExists(x => x.FindElement(By.TagName(tagName)), seconds);
+        await driver.WaitElementExists(x => x.FindElement(By.TagName(tagName)), seconds);
     }
-    public static void WaitElementExistsByLinkText(this IWebDriver driver, string linkText, int seconds = 15)
+    public static async Task WaitElementExistsByLinkText(this IWebDriver driver, string linkText, int seconds = 15)
     {
-        driver.WaitElementExists(x => x.FindElement(By.LinkText(linkText)), seconds);
+        await driver.WaitElementExists(x => x.FindElement(By.LinkText(linkText)), seconds);
     }
     private static async Task WaitElementExists<T>(this IWebDriver driver, Func<IWebDriver, T> getElement, int seconds = 15) where T : IWebElement
     {
@@ -104,10 +107,7 @@ public static class GCScriptExtensionMethods
         throw new Exception($"Limit of {seconds} seconds exceeded in [WaitElementExists]");
     }
 
-    public static string InnerText(this IWebElement element) => element.GetAttribute("innerText");
-    public static string InnerHTML(this IWebElement element) => element.GetAttribute("innerHTML");
-
-    public static string GetAlertText(this IWebDriver driver, int seconds = 15) { driver.WaitAlert(seconds); return driver.SwitchTo().Alert().Text; }
+    public static string GetAlertText(this IWebDriver driver, int seconds = 15) => driver.SwitchTo().Alert().Text;
     public static async Task WaitAlert(this IWebDriver driver, int seconds = 15)
     {
         seconds = Math.Max(1, seconds);
@@ -122,8 +122,8 @@ public static class GCScriptExtensionMethods
         }
         throw new Exception($"Limit of {seconds} seconds exceeded in [WaitAlert]");
     }
-    public static void WaitAlertAndAccept(this IWebDriver driver, int seconds = 15) { driver.WaitAlert(seconds); driver.SwitchTo().Alert().Accept(); }
-    public static void WaitAlertAndDismiss(this IWebDriver driver, int seconds = 15) { driver.WaitAlert(seconds); driver.SwitchTo().Alert().Dismiss(); }
+    public static async Task WaitAlertAndAccept(this IWebDriver driver, int seconds = 15) { await driver.WaitAlert(seconds); driver.SwitchTo().Alert().Accept(); }
+    public static async Task WaitAlertAndDismiss(this IWebDriver driver, int seconds = 15) { await driver.WaitAlert(seconds); driver.SwitchTo().Alert().Dismiss(); }
 
     public static bool PageSourceContainsText(this IWebDriver driver, string text) => driver.PageSource.Contains(text, StringComparison.OrdinalIgnoreCase);
     public static bool PageSourceNotContainsText(this IWebDriver driver, string text) => !driver.PageSource.Contains(text, StringComparison.OrdinalIgnoreCase);
@@ -222,4 +222,130 @@ public static class GCScriptExtensionMethods
         }
         throw new Exception($"Limit of {seconds} seconds exceeded in [WaitBodyNotContainsText]");
     }
+
+    public static string InnerText(this IWebElement element) => element.GetAttribute("innerText");
+    public static string InnerHTML(this IWebElement element) => element.GetAttribute("innerHTML");
+    public static void Focus(this IWebElement element)
+    {
+        IWebDriver driver = ((IWrapsDriver)element).WrappedDriver;
+        driver.ExecuteJavaScript("arguments[0].focus();", element);
+    }
+    public static void Unfocus(this IWebElement element)
+    {
+        IWebDriver driver = ((IWrapsDriver)element).WrappedDriver;
+        driver.ExecuteJavaScript("arguments[0].blur();", element);
+    }
+    public static async Task WaitForPageToLoad(this IWebDriver driver, int seconds = 15)
+    {
+        seconds = Math.Max(1, seconds);
+        for (int i = 0; i < seconds; i++)
+        {
+            if (driver.ExecuteJavaScript<bool>("return document.readyState === 'complete';"))
+            {
+                return;
+            }
+            await Task.Delay(1000);
+        }
+        throw new Exception($"Limit of {seconds} seconds exceeded in [WaitForPageToLoad]");
+    }
+    public static void OverrideAlertConfirmTrue(this IWebDriver driver) => driver.ExecuteJavaScript("window.confirm = function () { return true; };");
+
+    public static void FindWindowContainsTextInUrl(this IWebDriver driver, string text)
+    {
+        foreach (string window in driver.WindowHandles)
+        {
+            driver.SwitchTo().Window(window);
+            if (driver.Url.Contains(text, StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+        }
+        throw new Exception($"Unable to find window containing text [{text}] in URL");
+    }
+    public static void FindWindowContainsTextInTitle(this IWebDriver driver, string text)
+    {
+        foreach (string window in driver.WindowHandles)
+        {
+            driver.SwitchTo().Window(window);
+            if (driver.Title.Contains(text, StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+        }
+        throw new Exception($"Unable to find window containing text [{text}] in Title");
+    }
+    public static void FindWindowStartsWithTextInTitle(this IWebDriver driver, string text)
+    {
+        foreach (string window in driver.WindowHandles)
+        {
+            driver.SwitchTo().Window(window);
+            if (driver.Title.StartsWith(text, StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+        }
+        throw new Exception($"Unable to find window starting with text [{text}] in Title");
+    }
+    public static void FindWindowEndsWithTextInTitle(this IWebDriver driver, string text)
+    {
+        foreach (string window in driver.WindowHandles)
+        {
+            driver.SwitchTo().Window(window);
+            if (driver.Title.EndsWith(text, StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+        }
+        throw new Exception($"Unable to find window ending with text [{text}] in Title");
+    }
+
+    public static async Task WaitWindowHandlesCount(this IWebDriver driver, int count, int seconds = 15)
+    {
+        seconds = Math.Max(1, seconds);
+        for (int i = 0; i < seconds; i++)
+        {
+            if (driver.WindowHandles.Count == count)
+            {
+                return;
+            }
+            await Task.Delay(1000);
+        }
+        throw new Exception($"Limit of {seconds} seconds exceeded in [WaitWindowHandlesCount]");
+    }
+    public static async Task WaitWindowHandlesCountMoreThan(this IWebDriver driver, int count, int seconds = 15)
+    {
+        seconds = Math.Max(1, seconds);
+        for (int i = 0; i < seconds; i++)
+        {
+            if (driver.WindowHandles.Count > count)
+            {
+                return;
+            }
+            await Task.Delay(1000);
+        }
+        throw new Exception($"Limit of {seconds} seconds exceeded in [WaitWindowHandlesCountMoreThan]");
+    }
+
+    public static void NavigateToUrl(this IWebDriver driver, string url) => driver.Navigate().GoToUrl(url);
+    public static void NavigateToUrlWithJS(this IWebDriver driver, string url) => driver.ExecuteJavaScript($"window.location.href = '{url}';");
+
+    public static void SelectIndexInDropdown(this IWebElement element, int index) => new SelectElement(element).SelectByIndex(index);
+    public static void SelectTextInDropdown(this IWebElement element, string text) => new SelectElement(element).SelectByText(text);
+    public static void SelectValueInDropdown(this IWebElement element, string value) => new SelectElement(element).SelectByValue(value);
+
+    public static void SetValueWithJS(this IWebElement element, string value)
+    {
+        IWebDriver driver = ((IWrapsDriver)element).WrappedDriver;
+        driver.ExecuteJavaScript("arguments[0].value = arguments[1];", element, value);
+    }
+    public static string GetValueWithJS(this IWebElement element)
+    {
+        IWebDriver driver = ((IWrapsDriver)element).WrappedDriver;
+        return driver.ExecuteJavaScript<string>("return arguments[0].value;", element);
+    }
+
+
+    public static void ForceClose(this IWebDriver driver) { try { driver.Close(); } catch { } try { driver.Quit(); } catch { } try { driver.Dispose(); } catch { } }
+    public static void SaveScreenshot(this IWebDriver driver, string filePath) { try { driver.TakeScreenshot().SaveAsFile(filePath); } catch { } }
+    public static void SavePageSource(this IWebDriver driver, string filePath) { try { File.WriteAllText(filePath, driver.PageSource); } catch { } }
 }
